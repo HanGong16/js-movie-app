004fb0f5cce2319d285f2107dc6877dec9eae91a
gong16 <hgim96715@gmail.com>
2024-02-26 20:51:56 +0900

    feat:Add Headline

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,11 @@
 import { Component } from '../core/heropy.js';
+import Headline from '../components/Headline.js';
 
 export default class Home extends Component {
   render() {
-    this.el.innerHTML = '<h1>Hello</h1>';
+    const headline = new Headline().el;
+
+    this.el.classList.add('container');
+    this.el.append(headline);
   }
 }
