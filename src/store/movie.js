bf3ec782ffeb0c2359d842b1251d61e9bb800e2b
gong16 <hgim96715@gmail.com>
2024-02-29 20:35:40 +0900

    feat:Add MovieListMore

--- src/store/movie.js
+++ src/store/movie.js
@@ -5,20 +5,23 @@ const API_KEY = config.API_KEY;
 const store = new Store({
   searchText: '',
   page: 1,
+  pageMax: 1,
   movies: [],
 });
 
 export default store;
 
 export const searchMovies = async (page) => {
+  store.state.page = page;
   if (page === 1) {
-    store.state.page = 1;
     store.state.movies = [];
   }
   const res = await fetch(
     `https://www.omdbapi.com/?apikey=${API_KEY}&s=${store.state.searchText}&page=${page}`
   );
-  const { Search } = await res.json();
-  console.log(Search);
+  const { Search, totalResults } = await res.json();
+
   store.state.movies = [...store.state.movies, ...Search];
+
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
 };
