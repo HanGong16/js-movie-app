8eb87635bb51a21a6092f51f026aecd1569b1447
gong16 <hgim96715@gmail.com>
2024-02-29 20:51:10 +0900

    feat:Add loading Animation

--- src/store/movie.js
+++ src/store/movie.js
@@ -7,12 +7,14 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
+  loading: false,
 });
 
 export default store;
 
 export const searchMovies = async (page) => {
   store.state.page = page;
+  store.state.loading = true;
   if (page === 1) {
     store.state.movies = [];
   }
@@ -24,4 +26,5 @@ export const searchMovies = async (page) => {
   store.state.movies = [...store.state.movies, ...Search];
 
   store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+  store.state.loading = false;
 };
