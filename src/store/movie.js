a48f96d5583c0c2d7a832587755e4c91d1461022
gong16 <hgim96715@gmail.com>
2024-03-05 10:47:19 +0900

    feat:Add Error message

--- src/store/movie.js
+++ src/store/movie.js
@@ -8,6 +8,7 @@ const store = new Store({
   pageMax: 1,
   movies: [],
   loading: false,
+  message: 'Search for the movie title!',
 });
 
 export default store;
@@ -17,14 +18,23 @@ export const searchMovies = async (page) => {
   store.state.loading = true;
   if (page === 1) {
     store.state.movies = [];
+    store.state.message = '';
   }
-  const res = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${store.state.searchText}&page=${page}`
-  );
-  const { Search, totalResults } = await res.json();
-
-  store.state.movies = [...store.state.movies, ...Search];
-
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-  store.state.loading = false;
+  try {
+    const res = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${store.state.searchText}&page=${page}`
+    );
+    const { Search, totalResults, Response, Error } = await res.json();
+    if (Response === 'True') {
+      store.state.movies = [...store.state.movies, ...Search];
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+      store.state.pageMax = 1;
+    }
+  } catch (error) {
+    console.log('searchMovies error:', error);
+  } finally {
+    store.state.loading = false;
+  }
 };
