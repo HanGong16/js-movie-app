4f19fd9b3e03ca87072c9fab8a25e409f5243cc7
gong16 <hgim96715@gmail.com>
2024-03-08 07:38:27 +0900

    docs: Heropy.ts - Router
    1. rotues매개변수의 에러
    2. acc[key]의 에러
    3. routerView 에러
    4. currentRouter의 에러

--- src/core/heropy.ts
+++ src/core/heropy.ts
@@ -23,27 +23,38 @@ export class Component {
 }
 
 //Router
-function routeRender(routes) {
+interface Route{
+  path:string;
+  component: typeof Component;
+}
+type Routes=Route[]
+function routeRender(routes:Routes) {
   if (!location.hash) {
     history.replaceState(null, '', '/#/');
   }
   const routerView = document.querySelector('router-view');
   const [hash, queryString = ''] = location.hash.split('?');
+  interface Query{
+    [key:string]:string;
+  }
   const query = queryString.split('&').reduce((acc, cur) => {
     const [key, value] = cur.split('=');
     acc[key] = value;
     return acc;
-  }, {});
+  }, {} as Query);
   history.replaceState(query, '');
 
   const currentRoute = routes.find((route) =>
     new RegExp(`${route.path}/?$`).test(hash)
   );
-  routerView.innerHTML = '';
-  routerView.append(new currentRoute.component().el);
+  if(routerView){
+    routerView.innerHTML = '';
+    currentRoute && routerView.append(new currentRoute.component().el);
+  }
+  
   window.scrollTo(0, 0);
 }
-export function createRouter(routes) {
+export function createRouter(routes:Routes) {
   return function () {
     window.addEventListener('popstate', () => {
       routeRender(routes);
